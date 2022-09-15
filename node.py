5d14cbe09effdf8a503a9ec4ff7b01ba2964fff6
Felix <felix@felixleitner.com>
2022-09-15 15:05:06 +0100

    documentation

--- node.py
+++ node.py
@@ -1,18 +1,20 @@
 from player import *
 
 class Node:
-    def __init__():
+    def __init__(self):
         self.player = NonePlayer()
         self.structure = 0
-        self.neighbours = set()
+        self.neighbours = []
         self.resources = dict()
+        self.road_connections = dict()
 
-    def allot(self, num):
+    def allot(self, num:int) -> None:
         '''
         Allot the owner of a node the resources for a given dice roll
-        multiplied by the structure level (0-2) of the node
+        times the structure level (0-2) of the node
 
         num: the dice roll
         '''
         for r in self.resources[num]:
+            # gain 0 if empty, 1 if settlement, 2 if city
             self.player.hand[r] += self.structure
